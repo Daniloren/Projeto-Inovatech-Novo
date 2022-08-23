38944136e998de40e9c10698bb4d16b14fc48f8d
Daniella Furquim <daniella.lorenzano@gmail.com>
2022-08-23 16:24:27 -0300

    Atualização de login, matricula e python

---
+++ scriptinscricao.js
@@ -0,0 +1,9 @@
+function cadastrar() {
+    var incluir="Cadastro efetuado com sucesso";
+    alert(incluir);
+}
+
+function buscar() {
+    var pesquisar="busca não encontrada";
+    alert(pesquisar);
+}
